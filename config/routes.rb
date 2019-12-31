7950fc37536836b9365189886405e702e76fa90f
CaioFML <caio.felipemotta@hotmail.com>
2019-12-30 22:52:40 -0300

    Exchange Request Teste (obe)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'exchanges/index'
+  get 'exchanges/convert'
+
+  root "exchanges#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/requests/exchanges_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+
+RSpec.describe 'Exchanges', type: :request do
+  describe 'GET #index' do
+    it 'returns http success' do
+      get '/'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET #convert' do
+    let!(:amount) { rand(1..9999) }
+
+    it 'returns http success' do
+      get '/convert', params: {
+                        source_currency: "USD",
+                        target_currency: "BRL",
+                        amount: amount
+                      }
+      expect(response).to have_http_status(200)
+    end
+  end
+end
