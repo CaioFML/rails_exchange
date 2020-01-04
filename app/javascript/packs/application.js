6356f480240022bb41136780a8d033803a9ea575
CaioFML <caio.felipemotta@hotmail.com>
2020-01-04 13:37:10 -0300

    Deploy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,10 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("bootstrap/dist/js/bootstrap.bundle")
+require("tether/dist/js/tether")
 
 //= require_tree .
+//= require rails_ujs
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
