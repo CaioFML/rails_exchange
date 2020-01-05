84b08ed9bbe8cdbc3e00620627b6a79b411c60c9
CaioFML <caio.felipemotta@hotmail.com>
2020-01-05 18:28:20 -0300

    implements new feature tests

--- spec/features/exchanges_index_system_spec.rb
+++ spec/features/exchanges_index_system_spec.rb
@@ -1,16 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "Exchange Currency Process", type: :feature, js: true do
+  before { visit '/' }
+
   it "shows exchange value" do
-    visit '/'
     within("#exchange_form") do
-      select('EUR', from: 'source_currency')
-      select('USD', from: 'target_currency')
+      select('USD', from: 'source_currency')
+      select('BRL', from: 'target_currency')
+      expect(page).to have_field('result', placeholder: 'Resultado')
+
       fill_in 'amount', with: rand(1..9999)
     end
-    click_button 'CONVERTER'
 
-    # save_and_open_page
-    expect(page).to have_content("value")
+    expect(page.find_field("result").value).to be_present
+  end
+
+  it "shows exchange value to BTC" do
+    within("#exchange_form") do
+      select('BRL', from: 'source_currency')
+      select('BTC', from: 'target_currency')
+      expect(page).to have_field('result', placeholder: 'Resultado')
+
+      fill_in 'amount', with: rand(1..9999)
+    end
+
+    expect(page.find_field("result").value).to be_present
+  end
+
+  it "changes between currency" do
+    within("#exchange_form") do
+      select('USD', from: 'source_currency')
+      select('BRL', from: 'target_currency')
+      expect(page).to have_field('result', placeholder: 'Resultado')
+    end
+
+    click_button "INVERTER MOEDAS"
+
+    expect(page.find_field("source_currency").value).to eq "BRL"
+    expect(page.find_field("target_currency").value).to eq "USD"
   end
 end
